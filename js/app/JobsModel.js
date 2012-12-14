73dee1d300fe5e675c037e1c7836c9b695fad796
Tomdye <tom@ursaminor.co.uk>
2012-12-14 17:26:54 +0000

    moved stuff

---
+++ js/app/JobsModel.js
@@ -0,0 +1,92 @@
+define([
+	"dojo/_base/declare",
+	"dijit/_Widget",
+	"dojo/request/script",
+	"dojo/on",
+	"dojo/_base/lang",
+	"dojo/store/Memory",
+	"dojo/Evented",
+	//"dojo/store/Observable",
+	"dojo/Deferred"
+], function (
+	declare,
+	_Widget,
+	script,
+	on,
+	lang,
+	Memory,
+	Evented,
+	//Observable,
+	Deferred
+) {
+	return declare("app.JobsModel", [_Widget, Evented], {
+
+		tree: "jobs[name,color,builds[timestamp,result,culprits[fullName]]]",
+		suffix: "api/json",
+		jsonpCallback: "jobsModelJsonpCallback",
+
+		store : null,
+
+		url: "",
+
+		_ticker: null,
+
+		constructor: function (args) {
+			lang.mixin(this, args);
+
+			/*this.store = new Memory({
+				idProperty: "name"
+			});*/
+		},
+
+		destroy: function () {
+			delete window[this.jsonpCallback];
+			this.inherited(arguments);
+		},
+
+		fetch: function () {
+			if (!this.url) {
+				console.error("No URL has been set");
+				return false;
+			}
+
+			var def = new Deferred();
+
+			//setup a jsonpCallbackFunc
+			//because this jsonp is a bitch
+			//for some reason!
+			window[this.jsonpCallback] = lang.hitch(this, "_fetchResponse", def);
+
+			script.get(this.url + this.suffix, {
+				query: {
+					jsonp: this.jsonpCallback,
+					tree: this.tree,
+					time: (new Date()).getTime()
+				}
+			});
+
+			return def;
+		},
+
+		startTicker: function (interval) {
+			var self = this;
+			this._ticker = setInterval(lang.hitch(this, "fetch"), interval);
+		},
+
+		stopTicker: function () {
+			clearInterval(this._ticker);
+		},
+
+		/*get: function (query) {
+			return this.store.query(query);
+		},*/
+
+		_fetchResponse: function (def, response) {
+			//this.store.setData(response.jobs);
+			this.emit("fetchResponse", response.jobs);
+			def.resolve(response.jobs);
+		}
+
+		//onFetchResponse: function (response) {}
+	});
+});
